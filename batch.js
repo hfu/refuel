3462305b10786df7d91e193fa799bbda5a0fc1ee
Hidenori FUJIMURA <hfu@users.noreply.github.com>
2018-07-19 10:34:20 -0400

    update

--- batch.js
+++ batch.js
@@ -1,4 +1,5 @@
-const ts = 'rdcl railcl rvrcl anno nrpt nnfpt pfpt jhj fgd dem10b dem5a landformclassification1 landformclassification2'.split(' ')
+// const ts = 'rdcl railcl rvrcl anno nrpt nnfpt pfpt jhj fgd dem10b dem5a landformclassification1 landformclassification2'.split(' ')
+const ts = 'rvrcl anno nrpt nnfpt pfpt jhj'.split(' ')
 
 for (let t of ts) {
   console.log(`node index.js ${t} | node hst2mbtiles.js ${t}.mbtiles`)
