7ef9424e0d28793ad132165a8748d99735ed317b
Hidenori FUJIMURA <hfu@users.noreply.github.com>
2018-07-20 16:19:07 -0400

    update

--- README.md
+++ README.md
@@ -18,6 +18,29 @@ $ tile-join -o experimental.mbtiles *.mbtiles
 ```
 You need to have [tippecanoe](https://github.com/mapbox/tippecanoe) installed to run tile-join.
 
+package.json and batch.js explains the detail of the processing. In the case of large scale tileset, you may need to run the commands one by one. 
+
+### index.js
+index.js is the script to download GeoJSON vector tiles, convert them to vector tiles, and the store the vector tile to a line of [headless serialtiles](https://github.com/hfu/headless-serialtiles-spec).
+
+The basic usage is:
+```console
+$ node index.js fgd
+# this will download experimental_fgd tiles and convert them.
+```
+
+This will have a long-lasting connection to mokuroku.csv.gz. Therefore, in the case of large tileset, you may want to download mokuroku.csv.gz beforehand to avoid timeout to the connection to mokuroku.csv.gz.
+
+If you have fgd.mokuroku which is a copy of https://maps.gsi.go.jp/xyz/experimental_fgd/mokuroku.csv.gz, index.js can take the mokuroku as a second parameter. So, you can do as:
+```console
+$ node index.js fgd fgd.mokuroku
+```
+
+### hst2mbtiles.js
+hst2mbtiles.js is a script to create mbtiles from hst file. This is a product of a separate project called [hst2mbtiles](https://github.com/hfu/hst2mbtiles).
+
+index.js and hst2mbtiles can be piped, or the stored and the redirected.
+
 ## See also
 ### Technical elements
 - [binarian](https://github.com/hfu/binarian) is the basic code to convert a GeoJSON tile to vector tile.
--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const byline = require('byline')
+const fs = require('fs')
 const fetch = require('node-fetch')
 const zlib = require('zlib')
 const vtpbf = require('vt-pbf')
 const geojsonvt = require('geojson-vt')
 const moment = require('moment')
 
-if (process.argv.length !== 3) {
-  console.log('usage: node index.js nrpt | node hst2mbtiles.js nrpt.mbtiles')
+if (process.argv.length !== 3 && process.argv.length !== 4) {
+  console.log('usage: node index.js nrpt {mokuroku.csv.gz}| node hst2mbtiles.js nrpt.mbtiles')
   process.exit()
 }
 const t = process.argv[2]
 const stratify = require(`./stratify/${t}.js`)
 let count = 0
 let para = 0
-const PARAMAX = 30
+const PARAMAX = 10 // 60
 
 const show = (t, z, x, y) => {
   console.error(`${moment().format()}\t#${count}(${para})\t${t}/${z}/${x}/${y}`)
@@ -35,14 +36,18 @@ const refuel = (t, z, x, y, ttl, s) => {
 	).getTile(z, x, y)
       }
       // if (!o[t]) return
-      console.log(JSON.stringify({
-        z: z,
-        x: x,
-        y: y,
-        buffer: zlib.gzipSync(vtpbf.fromGeojsonVt(o, {version: 2}))
-          .toString('base64')
-      }))
-      count++
+      try {
+        console.log(JSON.stringify({
+          z: z,
+          x: x,
+          y: y,
+          buffer: zlib.gzipSync(vtpbf.fromGeojsonVt(o, {version: 2}))
+            .toString('base64')
+        }))
+        count++
+      } catch (e) {
+        console.error(e)
+      }
       para--
       if (para <= PARAMAX / 2) s.resume()
       if (count % 5000 === 0) show(t, z, x, y)
@@ -64,8 +69,27 @@ const refuel = (t, z, x, y, ttl, s) => {
     })
 }
 
-fetch(`https://maps.gsi.go.jp/xyz/experimental_${t}/mokuroku.csv.gz`)
-  .then(res => {
+const work = (stream) => {
+  const s = byline(stream.pipe(zlib.createGunzip()))
+  s.on('data', line => {
+    zxy = line.toString().split('/').map(v => parseInt(v))
+    if (zxy.length !== 3) return
+    const [z, x, y] = zxy
+    if (isNaN(z)) return
+    refuel(t, z, x, y, 20, s)
+  })
+}
+
+if (process.argv.length === 3) {
+  fetch(`https://maps.gsi.go.jp/xyz/experimental_${t}/mokuroku.csv.gz`)
+    .then(res => {
+      work(res.body)
+    })
+} else {
+  work(fs.createReadStream(process.argv[3]))
+}
+
+/*
     const s = byline(res.body.pipe(zlib.createGunzip()))
     s.on('data', line => {
       zxy = line.toString().split('/').map(v => parseInt(v))
@@ -76,4 +100,5 @@ fetch(`https://maps.gsi.go.jp/xyz/experimental_${t}/mokuroku.csv.gz`)
     })
     // s.on('pause', () => { process.stderr.write('p') })
     // s.on('resume', () => { process.stderr.write('r') })
-  })
+  }
+  */
